28dc8bcea14ea0c90f5f558c8114820f2f2aacba
Patroxclus <euphoricallensalvador@gmail.com>
2024-04-04 15:24:15 -0500

    added CreateLimbControl class

--- src/CreateController.py
+++ src/CreateController.py
@@ -2,7 +2,18 @@ import maya.cmds as mc
 
 from PySide2.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton
 
-class CreateControllerWidget(QWidget):
+class CreateLimbControl:
+    def __init__(self):
+        self.root = ""
+        self.mid = ""
+        self.end = ""
+
+    def FindJntsBasedOnRootSel(self):
+        self.root = mc.ls(sl=True, type = "joint")[0]
+        self.mid = mc.listRelatives(self.root, c=True, type="joint")[0]
+        self.end = mc.listRelatives(self.end, c=True, type ="joint")[0]
+
+class CreateLimbControllerWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Create IKFK limb")
@@ -24,5 +35,5 @@ class CreateControllerWidget(QWidget):
 
 
 
-controllerWidget = CreateControllerWidget()
+controllerWidget = CreateLimbControllerWidget()
 controllerWidget.show()
